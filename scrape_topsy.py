c7d4146d462157a47fe763ed0bab57c3d98b59cf
jroehl <contact@johannroehl.de>
2015-06-25 13:16:15 +0200

    commented...

--- scrape_topsy.py
+++ scrape_topsy.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+Script for scraping topsy website.
+
+Functions:
+- Navigate to topsy URL passed as parameter
+- Grab tweet details (TweetID, TweetText (including Hashtags, Links etc.), TweetTimestamp, InReplyTo ID)
+- Open www.gettwitterid.com at the end of the run to obtain twitter id
+- Save the details in standard scheme of tweets (adding None, or -1 for not yet obtained details)
+"""
 
 import re
 import csv
@@ -12,23 +21,28 @@ from selenium.webdriver import ActionChains
 
 
 def new_set():
+    """ Create new set."""
+    # only needed because var = set() does throw an error if used in main method
     var = set()
     return var
 
 
 def scrape(arg):
-
-    # return arg
-    # lists = scrape()
-    # write_to_CSV("scraped", lists)
+    """ Create new set."""
+    # if link does not start with "http://", it is added
     if not arg.startswith('http://'):
         arg = "http://{0}" .format(arg)
 
+    # new browser driver instance for Firefox
     browser = webdriver.Firefox()
+    # implicit wait for sites to load
     browser.implicitly_wait(2.5)
+    # navigate to page
     browser.get("{0}" .format(arg))
-    # browser.get('http://topsy.com/s?q=from%3ALOTR&mintime=1388617248')
+
+    # grabbed tweet count
     count = 0
+    # dictionary for storing the details and script data
     dic = {  # 10
            "tweet_id": new_set(),
              # 11
@@ -51,36 +65,52 @@ def scrape(arg):
            "favorite_count": [],
              # 8
            "text": [],
+             # 9
            "count": count,
+             # 10
            "errors": {"count": 0, "text": []}
            }
 
+    # the length of the "tweet_id" set (unique) of the previous run is stored in old_len for comparison with the new_len
     old_len = -1
     new_len = 1
-    # while count % 10 == 0:
+    # if the lenghts of the sets are equal, no new data has been obtained
     while old_len != new_len:
+
         old_len = len(dic["tweet_id"])
         new_dic = scrape_topsy(browser, dic)
         new_len = len(new_dic["tweet_id"])
-        # if old_len == new_len:
-        #     break
+        # break out of while loop to avoid duplicates
+        if old_len == new_len:
+            break
+
+        # if new results are found result is stored in the main dictionary
         dic = new_dic
+
         count = dic["count"]
+
+        # Try to find the "next" button on topsy page for navigation
         xpath = "//*[@id='module-pager']/div/ul/li[12]/a"
         for counter in range(3):
+            # page_has_loaded() method returns true (false) if xpath is (not) found
             if not page_has_loaded(browser, xpath):
+                # if this happens twice, URL is probably not a topsy URL
                 if counter == 2:
                     log("\"{1}\" - is not a topsy URL" .format(time.ctime(), arg, e))
                     sys.exit(0)
+                # browser is refreshed for new try
                 browser.refresh()
             else:
                 break
 
+        # next button is located and clicked for new result page
         next_btn = browser.find_element_by_xpath(xpath)
         next_btn.click()
 
+    # set is converted to list for next steps
     dic["tweet_id"] = list(dic["tweet_id"])
 
+    # header for the csv file
     header = ["Coordinates",
               "CreatedAt",
               "FavoriteCount",
@@ -94,31 +124,41 @@ def scrape(arg):
               "TwitterID"
               ]
 
+    # new write_list is created for the write_to_CSV() method
     write_list = []
+    # headerrow is appended as first entry
     write_list.append(header)
 
+    # navigate to URL to obtain the twitterID by the twitterName
     browser.get("http://gettwitterid.com/")
     name = dic["twitter_id"][0]
     search_bar = browser.find_element_by_xpath("//*[@id='search_bar']")
     search_bar.send_keys(name)
     search_bar.send_keys(Keys.RETURN)
-
     result = browser.find_element_by_xpath("/html/body/div/div[1]/table/tbody/tr[1]/td[2]/p").text
 
+    # overwrite the twitterName by the twitterID
     dic["twitter_id"] = [result for item in dic["twitter_id"]]
 
+    # save the errorcount and errortexts in temp variables
     errorcount = dic["errors"]["count"]
     errortexts = dic["errors"]["text"]
 
+    # delete the count an error dictionary entries for output
     del dic["count"]
     del dic["errors"]
+
+    # for every entry in the
     for count in range(len(dic["tweet_id"])):
         write_list.append([dic[key][count] for key in sorted(dic)])
 
+    # output as csv
     write_to_CSV(name, write_list)
 
+    # close the current firefox driver instance
     browser.quit()
 
+    # write logs for output through applescript notification
     if errorcount == 0:
         log("{0}\n{1} tweets scraped and saved to {2}.csv.." .format(time.ctime(), count+1, name))
     elif errorcount > 0:
@@ -129,9 +169,11 @@ def scrape(arg):
 
 
 def scrape_topsy(browser, set):
-
+    """Scrape the browser page_source."""
+    # get the whole content of page
     html_source = browser.page_source
 
+    # BeautifulSoup find only the media body tag
     soup = BeautifulSoup(html_source)
     results = soup.find_all("div", class_="media-body")
 
@@ -140,27 +182,34 @@ def scrape_topsy(browser, set):
         try:
             count += 1
 
+            # find <div> with tweet text in result
             text = result.find("div")
             text = tweet_regex(str(text))
 
+            # find <a> with twitter_id and strip ID
             twitter_id = result.find("a")
             twitter_id = re.search('(?<=twitter.com/).*?(")', str(twitter_id)).group()
             twitter_id = twitter_id.replace("\"", "")
 
+            # search vor <class=muted> in <a>
             muted = result.find_all("a", class_="muted")
+            # try to obtain the reply_to, if not found catch group().. exception and set rep_to = 0
             try:
                 rep_to = re.search('(?<=status/)\d+', str(muted)).group()
             except:
                 rep_to = 0
 
+            # find tweet_id and strip ID
             tweet_id = re.search('(?<=status/)\d+', str(muted)).group()
 
+            # find date as epoch time and convert to formatted date
             created_at = re.search('(?<=timestamp=")\d+', str(muted)).group()
             created_at = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(created_at)))
 
             # counts = browse_twitter(browser, result, count)
             counts = [-1, -1]
 
+            # save results to the dictionary, append None, or default values if not found
             set["tweet_id"].add(tweet_id)
             set["twitter_id"].append(twitter_id)
             set["created_at"].append(created_at)
@@ -173,6 +222,8 @@ def scrape_topsy(browser, set):
             set["favorite_count"].append(counts[1])
             set["text"].append(text)
             set["count"] = count
+
+        # catch exception of run, store errorcount increment and resulttext in dictionary["errors"]
         except:
             set["errors"]["count"] += 1
             set["errors"]["text"].append(result)
@@ -181,75 +232,94 @@ def scrape_topsy(browser, set):
 
 
 def page_has_loaded(browser, xpath):
-        try:
-            time.sleep(0.5)
-            browser.find_element_by_xpath(xpath)
-            return True
-        except:
-            return False
+    """Find out if page element could be found(page has loaded)."""
+    # returns true or false
+    try:
+        # sleep for half a second
+        time.sleep(0.5)
+        # try to find element
+        browser.find_element_by_xpath(xpath)
+        return True
+    except:
+        # catch ElementNotFoundException (page has not loaded)
+        return False
 
 
 def browse_twitter(browser, result, count):
-
+    """Open tweet page on twitter.com according to found tweet ID on topsy.com."""
     try:
+        # save current handle in variable
         main_window = browser.current_window_handle
     except Exception as e:
-        log("\n{0} current_window_handle" .format(e))
+        # catch if not found
+        errorlog("\n{0} current_window_handle" .format(e))
+        # return default value
         return [-1, -1]
 
+    # problem of skipped tweets is handled
+    # count for finding xpath is incremented, for other results than, tweets
     if count >= 6:
         count += 1
     try:
         tweet = browser.find_element_by_xpath("//*[@id='results']/div[{0}]/div/div/ul/li[1]/small/a/span[2]" .format(count))
     except:
-        try:
-            tweet = browser.find_element_by_xpath("//*[@id='results']/div[{0}]/div/div/ul/li[1]/small/a/span[2]" .format(count+1))
-        except:
-            return [-1, -1]
+        # if element not found return default value
+        return [-1, -1]
 
+    # action chain command + click to open twitter.com in new tab
     action = ActionChains(browser)
     action.key_down(Keys.COMMAND)
     action.click(tweet)
     action.key_up(Keys.COMMAND)
     action.perform()
 
+    # switch to new tab control + tab
     browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
 
+    # Different approach to handle page not loaded
     # Try
     # WebDriverWait(browser, 10).until(
     #     EC.presence_of_element_located((By.XPATH, "//*[@id='doc']"))
     # )
 
+    # try for 3 time to find the element on page (if page load was not fast enough)
     for counter in range(3):
         if not page_has_loaded(browser, "//*[@id='doc']"):
             if counter == 2:
-                # header = browser.find_element_by_xpath("/html/body/div[2]/div/h1").text
+                # after 3 tries return default value
                 browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
                 browser.switch_to_window(main_window)
                 return [-1, -1]
             browser.refresh()
 
+    # get the current browser content (of new tab)
     html_source = browser.page_source
     soup = BeautifulSoup(html_source)
+    # find part with retweet and favorite count number
     retweet_count = soup.find_all(class_="request-retweeted-popup")
+    # try to find retweet count if not return default
     try:
         retweet_count = re.search('(?<=<strong>)\d+', str(retweet_count)).group()
     except:
         retweet_count = -1
     favorite_count = soup.find_all(class_="request-favorited-popup")
+    # try to find favorite count if not return default
     try:
         favorite_count = re.search('(?<=<strong>)\d+', str(favorite_count)).group()
     except:
         favorite_count = -1
 
+    # focus on current main page (current tab) and close command + w
     browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
 
+    # switch focus to original (main) window handle
     browser.switch_to_window(main_window)
 
     return [retweet_count, favorite_count]
 
 
 def tweet_regex(string):
+    """Strip tweet from html codes."""
     tweet = re.sub('==.jsp', '', string)
     tweet = re.sub('<div>', '', tweet)
     tweet = re.sub('</div>', '', tweet)
@@ -281,10 +351,13 @@ def write_to_CSV(name, lists):
 
 
 def log(output):
+    """Write a log file into script directory."""
     with open("{0}/temp.log" .format(os.getcwd()), "a") as log:
         log.write("{0}\n" .format(output))
 
+
 def errorlog(output):
+    """Write an errorlog file into script directory."""
     with open("{0}/error.log" .format(os.getcwd()), "a") as log:
         log.write("{0}\n" .format(output))
 
@@ -298,6 +371,7 @@ def mk_dir(dirname):
 
 
 if __name__ == "__main__":
+    """Checks if no parameter is passed"""
     try:
         scrape(sys.argv[1])
     except Exception as e:
